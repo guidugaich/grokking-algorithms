f11c2a74fddd51907e98ff96769eaa5dbc23c255
guidugaich <guidugaich.dev@gmail.com>
2024-08-07 09:14:02 -0300

    bfs file search recursive

--- bfs_file_search.py
+++ bfs_file_search.py
@@ -7,16 +7,22 @@ def print_file_names(start_dir):
   search_queue.append(start_dir)
   while search_queue:
     print('search_queue', search_queue)
-    dir = search_queue.popleft()
-    if isfile(dir):
-      print(dir)
-    else:  
-      for file in sorted(listdir(dir)):
-        path = join(dir, file)
-        if isfile(file):
-          print(file)
-        else:
-          search_queue.append(path)
+    dir = search_queue.popleft() 
+    for file in sorted(listdir(dir)):
+      path = join(dir, file)
+      if isfile(path):
+        print(file)
+      else:
+        search_queue.append(path)
+
+def print_file_names_recursive(dir):
+  for file in sorted(listdir(dir)):
+    path = join(dir, file)
+    if isfile(path):
+      print(file)
+    else:
+      print_file_names_recursive(path)
+
 
 
 print_file_names('/home/remessaonline/projects/grokking-algorithms')
